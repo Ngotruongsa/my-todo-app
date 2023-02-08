b1b37d3ad5882c6d3bfc4f903d9d7341dd28091e
Ngô Đăng Trường Sa <Ngotruongsa2002@gmail.com>
2023-02-08 21:37:47 +0700

    first commit

---
+++ src/components/layout/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class Header extends React.Component {
+    render() {
+        return ( 
+            <header class ="header-container">
+                <h1 class ="header-title"> Simple Todo App </h1> 
+            </header >
+        )
+    }
+}
+export default Header;
